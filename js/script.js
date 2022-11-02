10087ac93bcd2cc31caa690c419cd7c2917d7b4d
alexNovik <alexnovik86@gmail.com>
2022-11-02 23:09:44 +0300

    main layout part_1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+//Меню бургер
 
+const iconMenu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (event) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
